9dee9c8c0634dca81ba5cf1d98d711bc91d79439
nyfea <izah.oliveira21@gmail.com>
2019-05-13 17:19:38 -0300

    inicio

---
+++ ToDo/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, NavPush, Button } from 'ionic-angular';
+
+/**
+ * Generated class for the HelloIonicPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html',
+})
+export class HelloIonicPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HelloIonicPage');
+  }
+
+}
